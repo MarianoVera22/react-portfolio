1f1088670326f6e2ba31b96e433e5860576c2377
Mariano Nicolas Vera Causich <marianonvera.22@gmail.com>
2024-02-06 17:20:59 -0300

    contendio Home

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -6,12 +6,15 @@ const Data = () => {
         <h1 className="home_title">
             Mariano Vera Causich
         </h1>
-        <h3 className="home_subitle">
+        <h3 className="home_subtitle">
             Ingeniero Electrónico / Programador
         </h3>
         <p className="home_description">
             Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rem expedita unde maxime ad mollitia tenetur voluptate, dolorem dolor eveniet impedit omnis quasi perferendis ea qui quaerat, repellat praesentium. Provident, adipisci!
         </p>
+        <button className='home_button'>
+          Descargar CV
+        </button>
     </div>
   )
 }
