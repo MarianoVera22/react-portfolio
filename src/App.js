a1889cd7979766ff4081b339e33280194ea0839c
Mariano Nicolas Vera Causich <marianonvera.22@gmail.com>
2024-02-17 09:32:42 -0300

    correccion route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Nav from './components/nav/Nav.jsx';
 import Home from './components/home/Home.jsx';
